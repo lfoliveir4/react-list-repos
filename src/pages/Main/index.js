3ad93baa536153e97e078ec0f73e99b10b94e520
Luis Filipe Alves de Oliveira <lfoliveira.dev@gmail.com>
2020-01-31 03:08:24 -0300

    eslint on state

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/state-in-constructor */
 import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
